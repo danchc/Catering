2e3b5faaf5f6fb49c34eece961e53fced20aec77
Daniel <85527001+danchc@users.noreply.github.com>
2022-05-19 10:17:47 +0200

    Effettuati vari aggiustamenti
    Aggiunta classe validator per la registrazione, ancora da implementare in maniera corretta

--- src/main/java/it/uniroma3/siw/spring/controller/AuthController.java
+++ src/main/java/it/uniroma3/siw/spring/controller/AuthController.java
@@ -1,24 +1,53 @@
 package it.uniroma3.siw.spring.controller;
 
+import it.uniroma3.siw.spring.controller.validator.CredentialsValidator;
 import it.uniroma3.siw.spring.model.Credentials;
 import it.uniroma3.siw.spring.model.User;
+import it.uniroma3.siw.spring.service.CredentialsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
 
+    //injection
+    @Autowired
+    protected CredentialsValidator credentialsValidator;
+
+    @Autowired
+    protected CredentialsService credentialsService;
+
     /**
      * Il metodo gestisce il GET per andare nella pagina della registrazione.
      * @param model
-     * @return register -> register.html
+     * @return register-form -> register-form.html
      */
-    @GetMapping("/register")
+    @GetMapping("/register-form")
     public String getRegistrationPage(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("credentials", new Credentials());
-        return "register";
+        return "register-form";
+    }
+
+    @PostMapping("/register-form")
+    public String register(Model model,
+                           @ModelAttribute("user") User user,
+                           BindingResult userBinding,
+                           BindingResult credentialsBinding,
+                           @ModelAttribute("credentials") Credentials credentials){
+
+        this.credentialsValidator.validate(credentials, credentialsBinding);
+        if(!credentialsBinding.hasErrors()){
+            credentials.setUser(user);
+            this.credentialsService.save(credentials);
+            return "register-success";
+        }
+        return "register-form";
     }
 
 }
--- src/main/java/it/uniroma3/siw/spring/model/Credentials.java
+++ src/main/java/it/uniroma3/siw/spring/model/Credentials.java
@@ -31,7 +31,7 @@ public class Credentials {
     private String password;
 
     @NotNull
-    @Column(name = "confirmed-password")
+    @Column(name = "confirmed_password")
     private String confermaPassword;
 
     @NotNull
@@ -43,59 +43,4 @@ public class Credentials {
     @OneToOne(cascade = {CascadeType.ALL})
     private User user;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfermaPassword() {
-        return confermaPassword;
-    }
-
-    public void setConfermaPassword(String confermaPassword) {
-        this.confermaPassword = confermaPassword;
-    }
-
-    public String getRuolo() {
-        return ruolo;
-    }
-
-    public void setRuolo(String ruolo) {
-        this.ruolo = ruolo;
-    }
-
-    public boolean isEnabled() {
-        return isEnabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        isEnabled = enabled;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
