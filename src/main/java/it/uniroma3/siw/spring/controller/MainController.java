1a7b170cb214c4b9336603384e6f2eddf077395e
Daniel <85527001+danchc@users.noreply.github.com>
2022-06-01 22:57:02 +0200

    Aggiunta pagina account ed effettuati fix
    Modificato css e java

--- src/main/java/it/uniroma3/siw/spring/controller/MainController.java
+++ src/main/java/it/uniroma3/siw/spring/controller/MainController.java
@@ -1,11 +1,19 @@
 package it.uniroma3.siw.spring.controller;
 
+import it.uniroma3.siw.spring.model.Credentials;
+import it.uniroma3.siw.spring.service.CredentialsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private CredentialsService credentialsService;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -15,5 +23,13 @@ public class MainController {
     @GetMapping("/buffets")
     public String getBuffets(Model model) {
         return "buffets";
+    }
+
+    @GetMapping("/user")
+    public String getUserProfile(Model model){
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
+        model.addAttribute(credentials);
+        return "userprofile";
     }
 }
